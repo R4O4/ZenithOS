e042eee6626e2aa34398f839c6c780aafa7e4ff8
Romain404 <139792234+R4O4@users.noreply.github.com>
2024-06-02 22:26:25 +0200

    Remove one desktop shortcut

--- components/shortcuts.php
+++ components/shortcuts.php
@@ -3,8 +3,4 @@
         <img src="icons/folder.svg" alt="folder">
         <p class="shortcut-label">Documents</p>
     </div>
-    <div class="zenithos-desktop-shortcut">
-        <img src="icons/folder.svg" alt="folder">
-        <p class="shortcut-label">Des trucs</p>
-    </div>
 </section>
