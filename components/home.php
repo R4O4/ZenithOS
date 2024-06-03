3f76f5b6dba792d45820851ab2f3c6ff4f040a5d
Romain404 <139792234+R4O4@users.noreply.github.com>
2024-06-03 13:08:46 +0200

    Reset to switch to react

--- components/home.php
+++
@@ -1,4 +0,0 @@
-<section class="desktop-home" style="display: none;">
-    <p style="color: #fff">John Doe</p><br/>
-    <button class="home-power-button"><img src="icons/power.svg" alt="powerButton"></button>
-</section>
--- components/shortcuts.php
+++
@@ -1,6 +0,0 @@
-<section class="zenithos-desktop-shortcuts">
-    <div class="zenithos-desktop-shortcut">
-        <img src="icons/folder.svg" alt="folder">
-        <p class="shortcut-label">Documents</p>
-    </div>
-</section>
